0e71ce5f2f9df8b5febaf996788ac6d28524c916
Abdullah Shishir <abdullahshishir786@gmail.com>
2025-02-01 03:20:22 +0600

    certifications added

--- js/script.js
+++ js/script.js
@@ -16,6 +16,20 @@ window.onscroll = () => {
 };
 
 /*========== swiper ==========*/
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 50,
+  loop: true,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
 /*========== dark light mode ==========*/
 
